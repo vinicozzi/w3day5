b8b50687044a619bda1cdaecca4641d11eedc405
vinicozzi <vinicozzimincent@gmail.com>
2023-03-24 14:22:58 -0400

    Add initialize Parent=

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -1,3 +1,27 @@
 class PolyTreeNode
-  
+
+    attr_accessor :parent, :children, :value 
+
+    def initialize(value)
+        @value = value 
+        @parent = nil 
+        @children = [] 
+    end 
+
+    def parent=(parent)
+        if !@parent.nil?
+            @parent.children.delete(self)
+        end 
+
+        @parent = parent  
+
+        if !parent.nil? 
+            parent.children << self if !parent.children.include?(self)
+        end 
+    end 
+
+    
+
+
+
 end
